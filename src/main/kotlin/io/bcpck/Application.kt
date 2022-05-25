5150c84c53ce0b9107b150431368e93d86ba8b8a
Herman Shpryhau <herman.shpryhau@gmail.com>
2022-05-25 19:56:50 +0300

    Initial commit

---
+++ src/main/kotlin/io/bcpck/Application.kt
@@ -0,0 +1,14 @@
+package io.bcpck
+
+import io.ktor.server.engine.*
+import io.ktor.server.netty.*
+import io.bcpck.plugins.*
+
+fun main() {
+    embeddedServer(Netty, port = 8080, host = "0.0.0.0") {
+        configureRouting()
+        configureSecurity()
+        configureHTTP()
+        configureSerialization()
+    }.start(wait = true)
+}
